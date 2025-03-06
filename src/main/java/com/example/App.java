2f9b14ae008fd1cec9d4e1721a990b6b89663865
Parisa Moshfegh <parisamoshfegh@Mac.hitronhub.home>
2025-03-06 10:52:42 -0800

    add and initialize chess board

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,15 +1,27 @@
 package com.example;
 
-public class App {
-    public int EMAI_INDEX = 1;
+import com.example.ChessBoard.Chess;
 
+public class App {
     public static void initializeChessPieces() {
         ChessBoard chessBoard = new ChessBoard();
-        chessBoard.printChessBoard();
+        Chess[][] standardBoard = chessBoard.initializeStandardBoard();
+        chessBoard.printChessBoard(standardBoard);
+
+        System.out.println();
+        System.out.println("******************************************************");
+        System.out.println("******************************************************");
+        System.out.println();
+
+        Chess[][] randomBoard = chessBoard.generateRandomBoard();
+        chessBoard.printChessBoard(randomBoard);
     }
 
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Hello World!");
+        System.out.println("Let's play some chess together");
+        System.out.println();
         initializeChessPieces();
     }
 }
--- src/main/java/com/example/ChessBoard.java
+++ src/main/java/com/example/ChessBoard.java
@@ -35,6 +35,40 @@ public class ChessBoard {
         }
     };
 
+    public Chess[][] initializeStandardBoard() {
+        Chess[][] board = new Chess[8][8];
+    
+        // Initialize White pieces
+        board[0][0] = new Chess(Color.WHITE, Peices.ROOK);
+        board[0][1] = new Chess(Color.WHITE, Peices.KNIGHT);
+        board[0][2] = new Chess(Color.WHITE, Peices.BISHOP);
+        board[0][3] = new Chess(Color.WHITE, Peices.QUEEN);
+        board[0][4] = new Chess(Color.WHITE, Peices.KING);
+        board[0][5] = new Chess(Color.WHITE, Peices.BISHOP);
+        board[0][6] = new Chess(Color.WHITE, Peices.KNIGHT);
+        board[0][7] = new Chess(Color.WHITE, Peices.ROOK);
+    
+        for (int col = 0; col < 8; col++) {
+            board[1][col] = new Chess(Color.WHITE, Peices.PAWN);
+        }
+    
+        // Initialize Black pieces
+        board[7][0] = new Chess(Color.BLACK, Peices.ROOK);
+        board[7][1] = new Chess(Color.BLACK, Peices.KNIGHT);
+        board[7][2] = new Chess(Color.BLACK, Peices.BISHOP);
+        board[7][3] = new Chess(Color.BLACK, Peices.QUEEN);
+        board[7][4] = new Chess(Color.BLACK, Peices.KING);
+        board[7][5] = new Chess(Color.BLACK, Peices.BISHOP);
+        board[7][6] = new Chess(Color.BLACK, Peices.KNIGHT);
+        board[7][7] = new Chess(Color.BLACK, Peices.ROOK);
+    
+        for (int col = 0; col < 8; col++) {
+            board[6][col] = new Chess(Color.BLACK, Peices.PAWN);
+        }
+    
+        return board;
+    }
+
     public List<Chess> generatePieces() {
         List<Chess> allPeices = new ArrayList<>();
         for (Color c : Color.values()) { 
@@ -53,35 +87,99 @@ public class ChessBoard {
         return allPeices;
     }
 
-    public Chess[][] generateBoard() {
+    public Chess[][] generateRandomBoard() {
         Chess[][] board = new Chess[8][8];
         List<Chess> shuffledList = generatePieces();
         Collections.shuffle(shuffledList);
-
-        int index = 0;
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                if (index < shuffledList.size()) {
-                    board[row][col] = shuffledList.get(index++);
-                }
+    
+        // Create a list of all available positions on the board (from (0,0) to (7,7))
+        List<int[]> positions = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                positions.add(new int[]{i, j});
             }
         }
+    
+        // Shuffle the list of positions to randomize where the pieces will go
+        Collections.shuffle(positions);
+        int index = 0;
+        for (int[] position : positions) {
+            int row = position[0];
+            int col = position[1];
+            if (index < shuffledList.size()) {
+                board[row][col] = shuffledList.get(index++);
+            }
+        }
+    
         return board;
     }
 
-    public void printChessBoard() {
-        Chess[][] chessBoard = generateBoard();
+    public void printChessBoard(Chess[][] chessBoard) {
+        System.out.println("    a   b   c   d   e   f   g   h");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
     
         for (int row = 0; row < chessBoard.length; row++) {
-            System.out.print((8 - row) + "  ");
+            System.out.print((8 - row) + " |");
             for (int col = 0; col < chessBoard[row].length; col++) {
                 if (chessBoard[row][col] == null) {
-                    System.out.print("EMPTY\t");
+                    System.out.print("   |");
                 } else {
-                    System.out.print(chessBoard[row][col] + "\t");
+                    Chess piece = chessBoard[row][col];
+                    String emoji;
+                    if (piece.getColor() == Color.WHITE) {
+                        switch (piece.getPeices()) {
+                            case KING:
+                                emoji = "♔";
+                                break;
+                            case QUEEN:
+                                emoji = "♕";
+                                break;
+                            case ROOK:
+                                emoji = "♖";
+                                break;
+                            case KNIGHT:
+                                emoji = "♘";
+                                break;
+                            case BISHOP:
+                                emoji = "♗";
+                                break;
+                            case PAWN:
+                                emoji = "♙";
+                                break;
+                            default:
+                                emoji = " ";
+                        }
+                    } else {
+                        switch (piece.getPeices()) {
+                            case KING:
+                                emoji = "♚";
+                                break;
+                            case QUEEN:
+                                emoji = "♛";
+                                break;
+                            case ROOK:
+                                emoji = "♜";
+                                break;
+                            case KNIGHT:
+                                emoji = "♞";
+                                break;
+                            case BISHOP:
+                                emoji = "♝";
+                                break;
+                            case PAWN:
+                                emoji = "♟";
+                                break;
+                            default:
+                                emoji = " ";
+                        }
+                    }
+    
+                    System.out.printf(" %s |", emoji);
                 }
             }
-            System.out.println();
+            System.out.println(" " + (8 - row));
+            System.out.println("  +---+---+---+---+---+---+---+---+");
         }
+        System.out.println("    a   b   c   d   e   f   g   h");
     }
 }
